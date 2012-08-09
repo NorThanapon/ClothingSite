8fc036610dc9cb995964c3de664072a73c7e0d41
Thanapon Noraset <flcl.tvboy@gmail.com>
2012-08-09 21:15:42 +0700

    add admin header again  - -a

---
+++ application/views/common/admin_header.php
@@ -0,0 +1,49 @@
+<div id="header">
+    <div id="header-wrapper" class="wrapper">
+        <header id="main-header">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">
+                            Order
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Report
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Brand
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Category
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Product
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Item
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Banner
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <a href="index.html" class="logo">
+                <h1>B-Fash: Administration</h1>
+            </a>
+        </header>
+    </div><!-- #header-wrapper .wrapper -->
+</div>
+<!-- #header -->
