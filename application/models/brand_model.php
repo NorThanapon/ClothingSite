e5eb2e8e2e62f3af6c85da0554dd0c89f0af14d0
Jidapa <Kraisangka>
2012-08-09 20:05:28 +0700

    brand_model.php
    Add function "Add","Edit,"Delete","View" for brand_management

--- application/models/brand_model.php
+++ application/models/brand_model.php
@@ -10,6 +10,38 @@ class Brand_model extends CI_Model {
     function __construct() {
         parent::__construct();
     }
+	
+	function add_brand()
+	{
+		$this->brand_name = $this->input->post('brand_name');
+		$this->description = $this->input->post('description');
+		$this->logo = $this->input->post('logo');
+		$this->size_chart = $this->input->post('size_chart');
+		
+		$this->db->insert('brands',$this);
+	}
+	
+	function edit_brand()
+	{
+		$this->brand_name = $this->input->post('brand_name');
+		$this->description = $this->input->post('description');
+		$this->logo = $this->input->post('logo');
+		$this->size_chart = $this->input->post('size_chart');
+		
+		$this->db->update('brands',$this,array('brand_name' => $_POST['brand_name']));
+	}
+	
+	function delete_brand()
+	{
+		$this->db->delete('brands',array('brand_name' => $_POST['brand_name']));
+	
+	
+	function view_brand()
+	{
+		$query = $this->db->get('brands');
+        return $query->result();
+	}
+	
 }
 
 ?>
