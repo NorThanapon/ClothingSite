a50bf1039d01e99058abf5380269d0186e391d43
pemika <zyth_neung@hotmail.com>
2012-08-09 23:03:53 +0700

    Brand.php - no change (i just test the program) brand_list.php - adding delete button

--- application/controllers/brand.php
+++ application/controllers/brand.php
@@ -5,8 +5,10 @@ class Brand extends CI_Controller {
     public function index() {
         $this->load->model('brand_model');
         $data['page_title'] = 'Admin: Brand Management';
-        $data['brand_list'] =  $this->brand_model->get_brand();
-	$this->load->view('brand/brand_list', $data);
+       
+
+ 	   $data['brand_list'] =  $this->brand_model->get_brand();
+	   $this->load->view('brand/brand_list', $data);
     }
     
 }
--- application/views/brand/brand_list.php
+++ application/views/brand/brand_list.php
@@ -5,10 +5,13 @@
     </head>
     <body>
         <?php $this->load->view('common/admin_header');?>
+		
+	<form id="brandName_form" action="brand_list.php"  method="post" >
 	<table border="1">
             <tr>
                 <td>No.</td>
                 <td>Brand Name</td>
+				<td></td>
             </tr>
             <?php 
                 $i =1;
@@ -16,7 +19,10 @@
             ?>
             <tr>
                 <td><?php echo $i;?></td>
-                <td><?php echo $items->brand_name."<br />";?></td>
+                <td><?php echo $items->brand_name;?> </td>
+				<td><input type="submit" name="delBrand" value="Delete" />
+					<input type="hidden" name="delSubmit" value="<?=$items->brand_name;?>" />
+				</td>
             </tr>
             <?php
                 $i++;
