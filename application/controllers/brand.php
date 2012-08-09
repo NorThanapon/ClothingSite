31668759d3919500bad34b619e487c2adfdccee9
Thanapon Noraset <flcl.tvboy@gmail.com>
2012-08-09 21:53:29 +0700

    update controller and model of brand

--- application/controllers/brand.php
+++ application/controllers/brand.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 class Brand extends CI_Controller {
@@ -17,4 +18,18 @@ class Brand extends CI_Controller {
 	
 }
 
+=======
+<?php
+
+class Brand extends CI_Controller {
+    public function index() {
+        $this->load->model('brand_model');
+        $data['page_title'] = 'Admin: Brand Management';
+        $data['brand_list'] =  $this->brand_model->get_brand();
+	$this->load->view('brand/brand_list', $data);
+    }
+    
+}
+
+>>>>>>> update controller and model of brand
 ?>
--- application/models/brand_model.php
+++ application/models/brand_model.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 class Brand_model extends CI_Model {
@@ -44,4 +45,57 @@ class Brand_model extends CI_Model {
 	
 }
 
+=======
+<?php
+
+class Brand_model extends CI_Model {
+
+    var $brand_name = '';
+    var $description = '';
+    var $logo = '';
+    var $size_chart = '';
+
+    function __construct() {
+        parent::__construct();
+    }
+	
+	function add_brand()
+	{
+		$this->brand_name = $this->input->post('brand_name');
+		$this->description = $this->input->post('description');
+		$this->logo = $this->input->post('logo');
+		$this->size_chart = $this->input->post('size_chart');
+		
+		$this->db->insert('brands',$this);
+	}
+	
+	function edit_brand()
+	{
+		$this->brand_name = $this->input->post('brand_name');
+		$this->description = $this->input->post('description');
+		$this->logo = $this->input->post('logo');
+		$this->size_chart = $this->input->post('size_chart');
+		
+		$this->db->update('brands',$this,array('brand_name' => $this->input->post('brand_name')));
+	}
+	
+	function delete_brand()
+	{
+		$this->db->delete('brands',array('brand_name' => $this->input->post('brand_name')));
+	}
+	
+	
+	function get_brand($brand_name = FALSE)
+	{
+	    if ($brand_name === FALSE) {
+		$query = $this->db->get('brands');	
+		return $query->result();
+	    }
+	    $query = $this->db->get_where('brands', array('brand_name' => $brand_name));
+	    return $query->row_array();
+	}
+	
+}
+
+>>>>>>> update controller and model of brand
 ?>
