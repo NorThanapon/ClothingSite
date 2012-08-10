97f71f7df920458bc436b5de3e76db9e5bc07cec
Thanapon Noraset <flcl.tvboy@gmail.com>
2012-08-10 15:56:57 +0700

    bug fixes on calling brand/index

--- application/controllers/brand.php
+++ application/controllers/brand.php
@@ -2,9 +2,10 @@
 <?php
 class Brand extends CI_Controller {
     public function index() {
-        if(check_authen('staff',TRUE))
+        if(!check_authen('staff',TRUE)) {
+            return;            
+        }
         $this->load->model('brand_model');
-        
         $data['page_title'] = 'Admin: Brand Management';
         $data['brand_list'] =  $this->brand_model->get_brand();
 	$this->load->view('brand/brand_list', $data);
