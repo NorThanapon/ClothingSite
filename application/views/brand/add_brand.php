bf8dd3d48022f7136b39c2bc3e83db3fb4678748
Thanapon Noraset <flcl.tvboy@gmail.com>
2012-08-11 00:43:50 +0700

    clear up the style and correct the indentation.
    there's still mess here. - -a

--- application/views/brand/add_brand.php
+++ application/views/brand/add_brand.php
@@ -3,65 +3,40 @@
     <head>
         <?php $this->load->view('common/head');?>
     </head>
-	
     <body>
         <?php $this->load->view('common/admin_header');?>
-	<form  >
-          <div style="border: thin solid #000000; margin: 10px; width: 460px;">
-     <table  
-            style="border-bottom-style: solid; border-bottom-width: thin; border-bottom-color: #000000" >
-			<tr >
-				<td valign="top" style="padding: 5px; width: 150px;" class="style4">
-					<strong style="color: #000066">Brand Name
-				</strong>
-				</td>
-				<td valign="baseline" align="left" 
-                    style="padding: 5px; width: 290px;">
-					<input name="brand_name" type ="text" style="width: 260px"/>
-				</td>
-			</tr>
-	</table>
-	<table style="border-bottom-style: solid; border-bottom-width: thin; border-bottom-color: #000000">
-			<tr >
-				<td align="justify" valign="top" style="padding: 5px ; width: 150px;" >
-					<strong style="color: #000066">Description 
-				</strong> 
-				</td>
-				<td align="left" class="style3" style="padding: 5px;width: 290px; ">
-					<textarea name ="description" style="width: 260px; height: 120px" ></textarea>
-				</td>
-			</tr>
-	</table>
-	<table style="border-bottom-style: solid; border-bottom-width: thin; border-bottom-color: #000000">
-			<tr>
-				<td valign="top" style="padding: 5px ;width: 150px; ">
-					<strong style="color: #000066">Logo 
-				</strong> 
-				</td>
-				<td align="left" class="style1" style="padding: 5px; width: 290px;">
-					<input name="logo " type ="file"/>
-				</td>
-			</tr>
-	</table>
-	<table style="border-bottom-style: solid; border-bottom-width: thin; border-bottom-color: #000000">
-			<tr>
-				<td valign="top" style="padding: 5px ;width: 150px; " >
-					<strong style="color: #000066" >Size Chart
-				</strong>
-				</td>
-				<td align="left" class="style1" style="padding: 5px; width: 290px;">
-					<input name="size_chart" type ="file"/>
-				</td>
-			</tr>
-	</table>
-	<div align="center"  style="padding: 5px; width: 460px;">
-					<input type = "submit" value="submit" align="top" style="width: 100px;" />
-	</div>
- 
-    </div>
-		
-	
+	<?php echo $error;?>
+	<?php echo form_open_multipart('brand/add');?>
+	    <div style="border: thin solid #000000; margin: 10px; width: 460px;">
+		<table>
+		    <tr >
+			<td>Brand Name</td>
+			<td><input name="brand_name" type ="text"/></td>
+		    </tr>
+		</table>
+		<table>
+		    <tr>
+			<td>Description</td>
+			<td><textarea name ="description"></textarea></td>
+		    </tr>
+		</table>
+		<table>
+		    <tr>
+			<td>Logo</td>
+			<td><input name="logo" type ="file"/></td>
+		    </tr>
+		</table>
+		<table>
+		    <tr>
+			<td>Size Chart</td>
+			<td><input name="size_chart" type ="file"/></td>
+		    </tr>
+		</table>
+		<div>
+		    <input type = "submit" value="submit"/>
+		</div>
+	    </div>
 	</form>
-    <?php $this->load->view('common/admin_footer');?>
+	<?php $this->load->view('common/admin_footer');?>
     </body>
 </html>
