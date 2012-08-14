6ee59a50d685d55508ccdc1d612a6ce99362c1f7
Thanapon Noraset <flcl.tvboy@gmail.com>
2012-08-15 05:06:27 +0700

    add table sorter, pager, and their style

---
+++ application/views/common/table_pager.php
@@ -0,0 +1,15 @@
+<div class="table-pager"> 
+    
+      <img src="<?php echo asset_url().'img/first.png';?>" class="first"/> 
+      <img src="<?php echo asset_url().'img/prev.png';?>" class="prev"/> 
+      <span type="text" class="pagedisplay"></span> <!-- this can be any element, including an input --> 
+      <img src="<?php echo asset_url().'img/next.png';?>" class="next"/> 
+      <img src="<?php echo asset_url().'img/last.png';?>" class="last"/> 
+      <select class="pagesize"> 
+        <option value="10">10</option> 
+        <option selected="selected" value="20">20</option> 
+        <option value="30">30</option> 
+        <option value="40">40</option> 
+      </select> 
+     
+</div>
