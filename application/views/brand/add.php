1491fae21a693cce38a176cd201842a289878faf
Thanapon Noraset <flcl.tvboy@gmail.com>
2012-08-22 22:38:20 +0700

    styling brand forms

--- application/views/brand/add.php
+++ application/views/brand/add.php
@@ -7,22 +7,25 @@
     <?php $this->load->view('common/admin_header');?>
 	<?php echo form_open_multipart('admin/brand/add');?>
 	<h1>Add brand</h1> 
-		<div>
-		    <?php $this->load->helper("form"); ?>
-			<label for="brand_name">Brand Name</label>
-			<input name="brand_name" value="<?php if(isset($brand_name)) echo $brand_name; ?>" type ="text"/>*
-			<?php echo form_error('brand_name', '<span class="form-error-message">', '</span>'); ?>
-			<br />
-			<label for="description">Description</label>
-			<textarea name ="description"  ><?php if(isset($form_description)) echo $form_description; ?></textarea>
-			<br />
-			<label for="logo">Logo</textarea>
-			<input name="logo" type ="file"/>
-			<br />
-			<?php echo anchor('admin/brand','Cancel' ,array('class' => 'button')); ?>
-			<input class="button btn-submit" type = "submit" name="submit" value="Add this brand"/>	
-		    </form>	
-		</div>
+	    <div class="form-input">
+		<fieldset>
+		    <legend>Brand Information</legend>
+		    <label for="brand_name">Brand Name:</label>
+		    <input name="brand_name" value="<?php if(isset($brand_name)) echo $brand_name; ?>" type ="text"/>*
+		    <?php echo form_error('brand_name', '<span class="form-error-message">', '</span>'); ?>
+		    <br />
+		    <label for="description">Description:</label>
+		    <textarea name ="description"  ><?php if(isset($form_description)) echo $form_description; ?></textarea>
+		    <br />
+		    <label for="logo">Logo:</label>
+		    <input name="logo" type ="file"/>
+		</fieldset>
+	    </div>
+	    <div class="form-action content-right">
+		<?php echo anchor('admin/brand','Cancel' ,array('class' => 'button')); ?>
+		<input class="button btn-submit" type = "submit" name="submit" value="Add this brand"/>
+	    </div>
+	</form>	
 	<?php $this->load->view('common/admin_footer');?>
     </body>
 </html>
--- application/views/brand/edit.php
+++ application/views/brand/edit.php
@@ -7,28 +7,32 @@
         <?php $this->load->view('common/admin_header');?>
 	<?php echo form_open_multipart('admin/brand/edit/'.$brand->brand_name);?>
 	<h1>Edit Brand</h1> 
-	    <div>
-		<input name="brand_name_key" type="hidden" value="<?php echo $brand->brand_name; ?>" />
-		<label for="brand_name" >Brand Name</label>
-		<input name="brand_name" type ="text" value="<?php if(isset($form_brand_name)) echo $form_brand_name; else echo $brand->brand_name; ?>" />*
-		<?php echo form_error('brand_name', '<span class="form-error-message">', '</span>'); ?>
-		<br />
-		<label for="description" >Description</label>
-		<textarea name ="description" ><?php if(isset($form_description)) echo $form_description; else echo $brand->description; ?></textarea>
-		<br />
-		<?php if ($brand->logo) { ?>
-		<img id="img-logo" src="<?php echo asset_url().'db/brands/'.$brand->logo; ?>" />
-		<br />
-		<span class="btn-minor-action" id="btn-remove-logo">remove logo</span>
-		<input type="hidden" name="remove_logo" id="hdn-remove-logo" value="" />
-		<br />
-		<?php } ?>
-		<label for="logo" >Logo</label>
-		<input name="logo" type ="file"/>
-		<br />							
+	    <div class="form-input">
+		<fieldset>
+		    <legend>Brand Information</legend>
+		    <input name="brand_name_key" type="hidden" value="<?php echo $brand->brand_name; ?>" />
+		    <label for="brand_name" >Brand Name:</label>
+		    <input name="brand_name" type ="text" value="<?php if(isset($form_brand_name)) echo $form_brand_name; else echo $brand->brand_name; ?>" />*
+		    <?php echo form_error('brand_name', '<span class="form-error-message">', '</span>'); ?>
+		    <br />
+		    <label for="description" >Description:</label>
+		    <textarea name ="description" ><?php if(isset($form_description)) echo $form_description; else echo $brand->description; ?></textarea>
+		    <br />
+		    <?php if ($brand->logo) { ?>
+		    <label>Current Logo:</label>
+		    <img id="img-logo" src="<?php echo asset_url().'db/brands/'.$brand->logo; ?>" />
+		    <br />
+		    <span class="btn-minor-action" id="btn-remove-logo">remove logo</span>
+		    <input type="hidden" name="remove_logo" id="hdn-remove-logo" value="" />
+		    <br />
+		    <?php } ?>
+		    <label for="logo" >Logo:</label>
+		    <input name="logo" type ="file"/>
+		</fieldset>
+	    </div>
+	    <div class="form-action content-right">
 		<?php echo anchor('admin/brand','Cancel' ,array('class' => 'button')); ?>
 		<input class="button btn-submit" type = "submit" name="submit" value="Save Change"/>	
-		<br />
 	    </div>
 	</form>
 	<?php $this->load->view('common/admin_footer');?>
