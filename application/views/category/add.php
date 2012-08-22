4ffad0e546b4b8e25c8100a6a2148e498035a4eb
Thanapon Noraset <flcl.tvboy@gmail.com>
2012-08-22 22:25:19 +0700

    styling category forms

--- application/views/category/add.php
+++ application/views/category/add.php
@@ -6,40 +6,43 @@
     <body>
     <?php $this->load->view('common/admin_header');?>
 	<?php echo form_open('admin/category/add'); ?>
-	<h1>Add Category</h1> 
-		<div>
-			<?php $this->load->helper("form"); ?>
-			<label for="cat_name_th">Name (Thai)</label>
-			<input name="cat_name_th" value="<?php if(isset($form_cat_name_th)) echo $form_cat_name_th; ?>" type ="text"/>*
-			<?php echo form_error('cat_name_th', '<span class="form-error-message">', '</span>'); ?>
-			<span class='form-error-message'><?php echo $dup_message_th;?></span>
-			<br />
-			<label for="cat_name_en">Name (English)</label>
-			<input name="cat_name_en" value="<?php if(isset($form_cat_name_en)) echo $form_cat_name_en; ?>" type ="text"/>*
-			<?php echo form_error('cat_name_en', '<span class="form-error-message">', '</span>'); ?>
-			<span class='form-error-message'><?php echo $dup_message_en;?></span>
-			<br />
-		    <label for="description_th">Description (Thai)</label>
-			<textarea name ="description_th" ><?php if(isset($description_th)) echo $description_th; ?></textarea>
-			<br />
-		    <label for="description_en">Description (English)</label>
-			<textarea name ="description_en" ><?php if(isset($description_en)) echo $description_en; ?></textarea>
-			<br />
-			<label for="cat_parent">Parent</textarea>
-			<select name="cat_parent" > 
-				<option value="">--None--</option>
-				<?php foreach($categories as $item)
-				{  ?>
-				
-					<option value="<?php echo $item->cat_id; ?>"><?php echo $item->cat_name_en; ?></option>
-				<?php 
-				}
-				?>
-			</select>
-			<br />
-			<?php echo anchor('admin/category','Cancel' ,array('class' => 'button')); ?>
-			<input class="button btn-submit" type = "submit" name="submit" value="Add this category"/>	
-		</div>	  
+	    <h1>Add Category</h1> 
+	    <div class="form-input">
+		<fieldset>
+		    <legend>Category Information</legend>
+		    <label for="cat_name_th">Name (Thai):</label>
+		    <input name="cat_name_th" value="<?php if(isset($form_cat_name_th)) echo $form_cat_name_th; ?>" type ="text"/>*
+		    <?php echo form_error('cat_name_th', '<span class="form-error-message">', '</span>'); ?>
+		    <span class='form-error-message'><?php echo $dup_message_th;?></span>
+		    <br />
+		    <label for="cat_name_en">Name (English):</label>
+		    <input name="cat_name_en" value="<?php if(isset($form_cat_name_en)) echo $form_cat_name_en; ?>" type ="text"/>*
+		    <?php echo form_error('cat_name_en', '<span class="form-error-message">', '</span>'); ?>
+		    <span class='form-error-message'><?php echo $dup_message_en;?></span>
+		    <br />
+		    <label for="description_th">Description (Thai):</label>
+		    <textarea name ="description_th" ><?php if(isset($description_th)) echo $description_th; ?></textarea>
+		    <br />
+		    <label for="description_en">Description (English):</label>
+		    <textarea name ="description_en" ><?php if(isset($description_en)) echo $description_en; ?></textarea>
+		    <br />
+		    <label for="cat_parent">Under Category:</label>
+		    <select name="cat_parent" > 
+			<option value="">--None--</option>
+			<?php foreach($categories as $item)
+			{  ?>
+			
+				<option value="<?php echo $item->cat_id; ?>"><?php echo $item->cat_name_en; ?></option>
+			<?php 
+			}
+			?>
+		    </select>
+		</fieldset>
+	    </div>
+	    <div class="content-right form-action">
+		<?php echo anchor('admin/category','Cancel' ,array('class' => 'button')); ?>
+		<input class="button btn-submit" type = "submit" name="submit" value="Add this category"/>
+	    </div>
 	</form>
 	<?php $this->load->view('common/admin_footer');?>
     </body>
--- application/views/category/edit.php
+++ application/views/category/edit.php
@@ -5,57 +5,53 @@
     </head>
     <body>
         <?php $this->load->view('common/admin_header');?>
-		<?php echo form_open('admin/category/edit/'.$categories->cat_id); ?>
-		<h1>Edit Category</h1> 
-			<div>
-				<input type="hidden" name="cat_id" value="<?php echo $categories->cat_id;?>" />
-				
-				<label for="cat_name_th" >Name (Thai)</label>
-				<input name="cat_name_th" type ="text" value="<?php echo $categories->cat_name_th; ?>" />*
-				<?php echo form_error('cat_name_th', '<span class="form-error-message">', '</span>'); ?>
-				<br />
-		
-				<label for="cat_name_en" >Name (English)</label>
-				<input name ="cat_name_en" type ="text" value="<?php echo $categories->cat_name_en; ?>" />*
-				<?php echo form_error('cat_name_en', '<span class="form-error-message">', '</span>'); ?>
-				<br />  
-				
-				<label for="description_th" >Description (Thai)</label>
-				<input name ="description_th" type ="text" value="<?php echo $categories->description_th; ?>" />
-				<br />
-				
-				<label for="description_en" >Description (English)</label>
-				<input name ="description_en" type ="text" value="<?php echo $categories->description_en; ?>" />
-				<br />
-			
-				<label for="cat_parent" >Parent</label>
-				<select name="cat_parent">
-					<option value="">--None--</option>
-					<?php 
-					$thisCat = $categories->cat_parent;
-					
-					foreach($allCat as $item)
-					{	
-						if($item->cat_id == $thisCat)
-						{
-							echo "<option value='".$item->cat_id."'  selected='selected'>".$item->cat_name_en."</option>";
-						}
-						if(!($item->cat_id == $categories->cat_id))
-						{
-							echo "<option value='".$item->cat_id."' >".$item->cat_name_en."</option>";
-						}
-					}	
-					
-					?>
-				</select>
-				<br />
-				
-				<?php echo anchor('admin/category','Cancel' ,array('class' => 'button')); ?>
-				<input class="button btn-submit" type = "submit" name="submit" value="Save Change"/>	
-				<br />
-				
-			</div>
-		</form>
-		<?php $this->load->view('common/admin_footer');?>
+	<?php echo form_open('admin/category/edit/'.$categories->cat_id); ?>
+	    <h1>Edit Category</h1> 
+	    <div class="form-input">
+		<fieldset>
+		    <legend>Category Information</legend>
+		    <input type="hidden" name="cat_id" value="<?php echo $categories->cat_id;?>" />
+		    <label for="cat_name_th" >Name (Thai):</label>
+		    <input name="cat_name_th" type ="text" value="<?php echo $categories->cat_name_th; ?>" />*
+		    <?php echo form_error('cat_name_th', '<span class="form-error-message">', '</span>'); ?>
+		    <br />
+		    <label for="cat_name_en" >Name (English):</label>
+		    <input name ="cat_name_en" type ="text" value="<?php echo $categories->cat_name_en; ?>" />*
+		    <?php echo form_error('cat_name_en', '<span class="form-error-message">', '</span>'); ?>
+		    <br />  
+		    <label for="description_th" >Description (Thai):</label>
+		    <input name ="description_th" type ="text" value="<?php echo $categories->description_th; ?>" />
+		    <br />
+		    <label for="description_en" >Description (English):</label>
+		    <input name ="description_en" type ="text" value="<?php echo $categories->description_en; ?>" />
+		    <br />
+		    <label for="cat_parent" >Under category:</label>
+		    <select name="cat_parent">
+			    <option value="">--None--</option>
+			    <?php 
+			    $thisCat = $categories->cat_parent;
+			    
+			    foreach($allCat as $item)
+			    {	
+				    if($item->cat_id == $thisCat)
+				    {
+					    echo "<option value='".$item->cat_id."'  selected='selected'>".$item->cat_name_en."</option>";
+				    }
+				    if(!($item->cat_id == $categories->cat_id))
+				    {
+					    echo "<option value='".$item->cat_id."' >".$item->cat_name_en."</option>";
+				    }
+			    }	
+			    
+			    ?>
+		    </select>
+		</fieldset>
+	    </div>
+	    <div class="form-action content-right">
+		<?php echo anchor('admin/category','Cancel' ,array('class' => 'button')); ?>
+		<input class="button btn-submit" type = "submit" name="submit" value="Save Change"/>	
+	    </div>
+	</form>
+	<?php $this->load->view('common/admin_footer');?>
     </body>
 </html>
