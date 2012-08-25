a8b51951c9c26198a250f71ee8e63db36f23cecb
Thanapon Noraset <flcl.tvboy@gmail.com>
2012-08-25 19:51:01 +0700

    addd blank file for the ui mock up

---
+++ application/views/common/head.php
@@ -0,0 +1,15 @@
+<title><?php echo $page_title; ?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="<?php echo asset_url().'css/reset-style.css';?>" />
+<link rel="stylesheet" href="<?php echo asset_url().'css/main.css';?>" />
+<link rel="stylesheet" href="<?php echo asset_url().'css/jqmodal.css';?>" />
+<script type="text/javascript" src="<?php echo asset_url().'js/jquery-1.7.1.min.js';?>" charset="utf-8"></script>     
+<script type="text/javascript" src="<?php echo asset_url().'js/jquery.jqmodal.js';?>" charset="utf-8"></script>
+<script type="text/javascript" src="<?php echo asset_url().'js/clothing-app.js';?>" charset="utf-8"></script>
+<!--[if gte IE 9]>
+<style type="text/css">
+    .gradient {
+        filter: none;
+    }
+</style>
+<![endif]-->
---
+++ application/views/main.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php $this->load->view('common/head'); ?>
+    </head>
+    <body>
+        <?php $this->load->view('common/header'); ?>
+        
+        <?php $this->load->view('common/footer'); ?>
+    </body>
+</html>
