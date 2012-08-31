f5b4de908d8a1406c6bd3d5011083ae0de4d1c5a
Thanapon Noraset <flcl.tvboy@gmail.com>
2012-08-31 22:39:51 +0700

    all image will be replace by no image.png (using javascript)

--- assets/js/clothing-app.js
+++ assets/js/clothing-app.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
 	$('#error-close-button').click(function () {
         $('.error-box').hide();
     });
-	
+    //add noimage
+    $('img').error(function() {
+        src = $(this).attr('src');
+        src = src.substring(0, src.indexOf('/assets/')+8) + 'img/noimage.png';
+        $(this).attr('src',src);
+    });
+    
     
 });
 
