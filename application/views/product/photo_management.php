c9027761cf4bac6a4a87f8d5abba98504a2ea133
Panisara <ae_1821@hotmail.com>
2012-09-16 19:34:15 +0700

    CSS Product photo_management

--- application/views/product/photo_management.php
+++ application/views/product/photo_management.php
@@ -10,24 +10,34 @@
 	    <h1>Photo of <?php echo $product->product_name_en; ?></h1> 
 	    <div class="form-input">
 		<fieldset>
-		    <legend>Management</legend>			
-			<br />
-		</fieldset>
-	    </div>
-		
-		<?php 
+		    <legend>Add Photos</legend>			
+			<?php echo form_open_multipart('admin/product/add_photo/', 'id="form-add-photo"'); ?>
+		<div id="add-photo">
+			<!--<div id="select-photo">-->
+		    <label for="photo">Photo:</label>
+		    <input name="photo" type ="file"/>
+			
+			<label for="color">Color:</label>			
+			<div id="select-color"><?php $this->load->view('common/color/color_picker');?>
+			
+			
+			<input type="hidden" name="product_id" value="<?php echo $product->product_id; ?>" />
+			<span class="button" id="btn-add-photo">Add Photo</span>
+						
+			
+		</div>
+		</fieldset>	
+		</form>
+	    <fieldset>
+			<legend>Existing</legend>
+			<?php 
 			foreach($photos as $item)
 			{
 		?>
-				<div class="report-items">
-					<img src="<?php echo asset_url().'db/products/'.$item->image_file_name."";?> "width="160" height="180" />		
+				<div class="report-items photo-item">
+					<img src="<?php echo asset_url().'db/products/'.$item->image_file_name."";?>" />		
 					<br />
-					<label for="<?php echo $item->image_id;?>"> color </label>
-					<img src="<?php echo asset_url().'db/colors/'.$item->color_file_name."";?> " />		
-					<label for="color_name"> <?php echo $item->color_name; ?> </label>
-					<br />
-					 Select color:
-                        <select class="color_picker" name="color_<?php echo $item->image_id ; ?>" id="ddl-color-<?php echo $item->image_id ; ?>" >
+					    <select class="color_picker" name="color_<?php echo $item->image_id ; ?>" id="ddl-color-<?php echo $item->image_id ; ?>" >
                             <option value="">-- Select a color --</option>
                             <?php
                                 foreach ($all_colors as $color) 
@@ -51,8 +61,8 @@
                                 }
                             ?>
                         </select>
-					<!--<?php echo anchor('admin/product/save_color/'.$item->image_id.'/'.$product->product_id, ' ', array('title'=>"Save Color",'class'=>'save-button')); ?>-->
-					save
+					
+					<?php echo anchor('admin/product/save_color/'.$item->image_id.'/'.$product->product_id, ' ', array('title'=>"Save Color",'class'=>'save-button')); ?>
 					<?php echo anchor('admin/product/delete_photo/'.$item->image_id.'/'.$product->product_id, ' ', array('title'=>"Delete Photo",'class'=>'delete-button')); ?>
 					<br />
 					
@@ -60,32 +70,23 @@
 		<?php		
 			}
 		?>	
-		
-		<?php echo form_open_multipart('admin/product/add_photo/'); ?>
-		<div>
-			<h1>Add Photo</h1>
-			<br />
-		    <label for="photo">Photo:</label>
-		    <input name="photo" type ="file"/>
-			<br />	
-			<label for="color">Color:</label>			
-			<?php $this->load->view('common/color/color_picker');?>
-			<br />
-			<input type="hidden" name="product_id" value="<?php echo $product->product_id; ?>" />
-			<input type = "submit" name="submit" value="Add photo" />					
-			
+		</fieldset>
 		</div>
+	
+		
 		
 	    <div class="content-right form-action">		
-			<input class="button btn-submit" type = "submit" name="submit" value="Save Change" />
-			<?php echo anchor('admin/product','Cancel' ,array('class' => 'button')); ?>	
+
+			<?php echo anchor('admin/product','Done' ,array('class' => 'button')); ?>	
 	    </div>
-	</form>
 	<?php $this->load->view('common/admin_footer');?>
 	<?php $this->load->view('common/confirm_box');?>
 	<?php $this->load->view('common/color/color_box');?>	
     </body>
 	<script type="text/javascript">
+	$("#btn-add-photo").click(function(){
+		document.getElementById("form-add-photo").submit();
+	});
 	<?php
 		foreach($photos as $item)
 		{
