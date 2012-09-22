ec44da743a8c402a21a8fcba79a308cb9c688710
pemika <zyth_neung@hotmail.com>
2012-09-22 22:23:24 +0700

    content-side+database

--- application/views/common/contentside.php
+++ application/views/common/contentside.php
@@ -11,16 +11,14 @@
 		</section>
 		<section class="side-navbox" id="side-navbox-brand">
 		<h2>BRANDS</h2>
-		<a href="###">BASIC DE NUVO</a><br />
-		<a href="###">BECKY RUSSELL</a><br />
-		<a href="###">BSC WOMEN</a><br />
-		<a href="###">ELLE</a><br />
-		<a href="###">ITOKIN BOUTIQUE</a><br />
-		<a href="###">MINNA</a><br />
-		<a href="###">MK MICHEL KLEIN</a><br />
-		<a href="###">NICE CAREER BANGKOK</a><br />
-		<a href="###">ST.ANDREWS</a><br />
-		<a href="###">ZARANYA</a><br />
+		<?php
+		foreach($brand_list as $item)
+		{
+		?>
+			<a href="#"><?php echo $item->brand_name;?></a><br />
+		<?php
+		}
+		?>
 		</section>
 		<!-- <section class="side-navbox" id="side-navbox-collection">
 		<h2>COLLECTION</h2>
