11d6c6adc98080145189ba0e87fcbecb3f6c1ac6
Nathatai <endlessdreams01@gmail.com>
2012-09-22 18:55:09 +0700

    test tag

--- application/views/tag/list.php
+++ application/views/tag/list.php
@@ -7,7 +7,7 @@
         <?php $this->load->view('common/admin_header');?>
 	<h1>Tag Management</h1>  
 	<div class="page-action">
-	    <?php echo anchor('admin/tag/add', '<img  src='.asset_url().'img/add-icon.png'.' />Add New tag', 'class="button gradient"'); ?>
+	    <span id="btn-add-tag" class="button gradient"><img  src="<?php echo asset_url().'img/add-icon.png';?>" />Add New tag</span>
 	</div>
 	<div class="clear-float"></div>
 	<div class="report-filter">
@@ -30,14 +30,71 @@
 				<th width="30">&nbsp;</th>
             </tr>
 		</thead>
-		<!--
-		<tbody>           
+		<tbody>
+            <?php 
+                $i =1;
+				foreach($tag_list as $item) 
+				{
+			?>
+			<tr>
+				<td><?php echo $item->tag_name_en;?> </td>              
+				<td><?php echo $item->tag_name_th?></td>				
+				<td><?php echo $item->total_quantity?></td>	
+				<td><?php if($item->isActive == 1) echo "show"; else echo "hide"; ?></td>
+				<td>
+					 <?php echo anchor('admin/tag/edit/'.$item->tag_id, ' ', array('title'=>"Edit Tag",'class'=>'edit-button')); ?>
+					 <?php echo anchor('admin/tag/delete/'.$item->tag_id, ' ', array('title'=>"Delete Tag",'class'=>'delete-button')); ?>
+				</td>
+			</tr>
+			<?php
+							
+                }
+			?>
 		</tbody>
-		-->
         </table>
 		<?php $this->load->view('common/table_pager');?>
 	</div>
 	</form>
-	</script>
+	<?php $this->load->view('common/admin_footer');?>
+	<?php $this->load->view('common/confirm_box');?>
+	<?php $this->load->view('common/tag_box');?>
     </body>
+	<script type="text/javascript">
+		 $(document).ready(function() {
+		//add confirm event for delete button
+		$('a.delete-button').click(function() { 
+		    confirm('Confirm for deletion','Do you want to delete this tag.',this.href, 'Delete'); 
+		    return false;
+		});
+		$('#btn_filter').click(function() {
+		    var url = document.URL;
+		    url = url.substring(0, url.indexOf('/tag') + 4);
+		    url = url + '/search/' + $('#txt_tag_name').val();
+		    window.location = url;
+		});
+
+		$("#btn-add-tag").click(function(){
+			add_tag()
+		});
+
+		$(".tablesorter").find("tr:even").addClass("even");
+		$(".tablesorter")
+		    .tablesorter({
+			headers: {
+				//0:{sorter:false},
+				6:{sorter:false},
+			}
+		    })
+		    .tablesorterPager({
+			container: $(".table-pager"),
+			positionFixed: false,
+			size:20
+		    });
+		$(".tablesorter").on('sortEnd', function(){
+		    //set striping color
+		    $(".tablesorter").find('tr').removeClass('even');
+		    $(".tablesorter").find("tr:even").addClass("even");
+		});
+	    }); 
+	</script>
 </html>
