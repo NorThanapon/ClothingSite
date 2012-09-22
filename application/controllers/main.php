6b66de5adf89d2cf56a3d2fae60f24ce09f69199
Panisara <ae_1821@hotmail.com>
2012-09-22 23:01:59 +0700

    Load Brand Model

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -3,6 +3,8 @@
 class Main extends CI_Controller {
     public function index() {
         $data['page_title'] = "Welcome to BfashShop.com";
+		$this->load->model('brand_model');
+		$data['brand_list'] = $this->brand_model->get(); 
         $this->load->view('main',$data);
     }
 }
--- application/controllers/productlist.php
+++ application/controllers/productlist.php
@@ -3,6 +3,8 @@
 class ProductList extends CI_Controller {
     public function index() {
         $data['page_title'] = "BfashShop.com";
+		$this->load->model('brand_model');
+		$data['brand_list'] = $this->brand_model->get(); 
 		$data['page'] = 'content-main-productlist';
         $this->load->view('main_page',$data);
     }
