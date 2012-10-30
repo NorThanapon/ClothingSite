764acb92e35195e2579cde6e49f61cc2064ce6db
Panisara <ae_1821@hotmail.com>
2012-10-31 01:11:12 +0700

    Add Payment Page

---
+++ application/controllers/payment.php
@@ -0,0 +1,15 @@
+<?php
+
+class Payment extends CI_Controller {
+	public function index() 
+	{
+       $data['page_title'] = 'Registration';
+
+		$data['error'] = "No";
+     
+		$data['page'] = 'Payment/payment';
+        $this->load->view('sub_page',$data);
+    }
+
+}
+?>
---
+++ application/views/Payment/payment.php
@@ -0,0 +1,26 @@
+<form id="step">
+  <fieldset title="Title">
+    <legend>description</legend>
+    <!-- input fields -->
+  </fieldset>
+
+  <fieldset title="Title">
+    <legend>description</legend>
+    <!-- input fields -->
+  </fieldset>
+
+  <input type="submit" class="finish"/>
+</form>
+<script type="text/javascript">
+			$(function() {
+$('#custom').stepy({
+  backLabel:      'Backward',
+  block:          true,
+  errorImage:     true,
+  nextLabel:      'Forward',
+  titleClick:     true,
+  validate:       true
+});
+
+});
+		</script>
