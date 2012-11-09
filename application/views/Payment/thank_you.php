0785a79b4d7b73fe3d9f7e4b6c31e780ce35bc16
Panisara <ae_1821@hotmail.com>
2012-11-10 02:20:36 +0700

    css thank you page

--- application/views/Payment/thank_you.php
+++ application/views/Payment/thank_you.php
@@ -1,32 +1,50 @@
-﻿<div id="thank_en" >
-THANK YOU FOR YOUR PURCHASE!
- 
-Thank you for your purchase! I hope you enjoy our products.
- 
-Your transaction has not yet been completed until the amount stated in the invoice has been wire-transferred to us.  Once the amount has been transferred, please upload your transfer receipt to us (or fax to 02-683-5791) by [DATE], and we will send you the receipt for your purchase via email.   You MUST put your name and the order number in the receipt.
- 
-You may log into your account to view details of this transaction.
- 
-If you have any questions about your order, or comments about your shopping experience, please contact us at support@bfashshop.com or call 02-xxx-xxxx.
- 
-Back to home
-Visit BFash.com
- 
-</div>
-<br />
-<div id="thank_th">
- 
-ขอบคุณสำหรับการซื้อสินค้าของท่าน
- 
-ขอบคุณสำหรับการซื้อสินค้าของท่าน เราหวังว่าท่านจะชอบสินค้าของเรา
- 
-การซื้อสินค้าของท่านยังไม่สิ้นสุดจนกว่าท่านจะโอนเงินมาให้เราตามจำนวนที่ได้ระบุไว้ในใบแจ้งหนี้ เมื่อท่านได้โอนเงินแล้วกรุณาส่งรูปถ่ายของใบเสร็จการโอนเงินจากธนาคารผ่านระบบ upload ของเราหรือแฟกซ์มาได้ที่ 02-683-5791 ภายในวันที่ [DATE] โดยเขียนชื่อและเลขที่ของการสั่งซื้อลงบนใบเสร็จการโอนเงินจากธนาคารด้วย เพื่อเราจะได้จัดส่งใบเสร็จการซื้อสินค้าให้แก่ท่าน
- 
-ท่านสามารถเข้าสู่ระบบบัญชีลูกค้าเพื่อดูรายละเอียดของการซื้อสินค้าครั้งนี้
- 
-ถ้าท่านมีคำถามเกี่ยวกับการซื้อสินค้าหรือมีข้อเสนอแนะเกี่ยวกับประสบการณ์การการซื้อสินค้าครั้งนี้ โปรดติดต่อเราได้ที่ support@bfashshop.com หรือโทร 02-xxx-xxxx
- 
-กลับสู่หน้าหลัก
-เข้าชม BFash.com
-</div>
-<input type="submit" class="btn_go_to_homepage" id="btn_go_to_homepage" name="btn_go_to_homepage" value="Continue Shopping" />
+﻿	<div id="thanku-head">
+		<div id="header-logo">
+			<?php echo anchor(base_url(), '<img src='.asset_url().'img/bfashshop.jpg />', 'title="BfashShop"'); ?>
+		</div>
+	</div>
+
+	<fieldset id="thanku">
+	<legend>THANK YOU FOR YOUR PURCHASE!</legend>
+		<div id="thank_en" >
+		
+		 
+		Thank you for your purchase! I hope you enjoy our products.<br /><br />
+		 
+		<p>Your transaction has not yet been completed until the amount stated in the invoice has been wire-transferred to us.
+
+		Once the amount has been transferred, please upload your transfer receipt to us (or fax to 02-683-5791) by [DATE], and we will send you the receipt for your purchase via email.
+		
+		You MUST put your name and the order number in the receipt.
+		 
+		You may log into your account to view details of this transaction.
+		 
+		If you have any questions about your order, or comments about your shopping experience, please contact us at support@bfashshop.com or call 02-xxx-xxxx.</p>
+		 
+		Back to home
+		Visit BFash.com
+		 
+		</div>
+		<br />
+	
+		 
+			<!-- <legend> ขอบคุณสำหรับการสั่งซื้อสินค้าของท่าน </legend>	-->
+		
+		<div id="thank_th">
+		
+		ขอบคุณสำหรับการสั่งซื้อสินค้าของท่าน เราหวังว่าท่านจะชอบสินค้าของเรา<br /><br />
+		 
+		<p>การซื้อสินค้าของท่านยังไม่สิ้นสุดจนกว่าท่านจะโอนเงินมาให้เราตามจำนวนที่ได้ระบุไว้ในใบแจ้งหนี้ เมื่อท่านได้โอนเงินแล้วกรุณาส่งรูปถ่ายของใบเสร็จการโอนเงินจากธนาคารผ่านระบบ upload ของเราหรือแฟกซ์มาได้ที่ 02-683-5791 ภายในวันที่ [DATE] โดยเขียนชื่อและเลขที่ของการสั่งซื้อลงบนใบเสร็จการโอนเงินจากธนาคารด้วย เพื่อเราจะได้จัดส่งใบเสร็จการซื้อสินค้าให้แก่ท่าน
+		 
+		ท่านสามารถเข้าสู่ระบบบัญชีลูกค้าเพื่อดูรายละเอียดของการซื้อสินค้าครั้งนี้
+		 
+		ถ้าท่านมีคำถามเกี่ยวกับการซื้อสินค้าหรือมีข้อเสนอแนะเกี่ยวกับประสบการณ์การการซื้อสินค้าครั้งนี้ โปรดติดต่อเราได้ที่ support@bfashshop.com หรือโทร 02-xxx-xxxx</p>
+		 
+		กลับสู่หน้าหลัก
+		เข้าชม BFash.com
+		</div>
+		<div id="btn_thanku">
+		<input type="submit" class="btn_go_to_homepage"  name="btn_go_to_homepage" value="Continue Shopping" />
+		<input type="submit" class="btn_print_invoice"  name="btn_print_invoice" value="Print Invoice" />
+		</div>
+	</fieldset>
